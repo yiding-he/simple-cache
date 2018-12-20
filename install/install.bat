3ee33461c9f470c3e114e2be001808edbcddde78
Yiding He <yiding.he@gmail.com>
2018-12-20 10:53:37 +0800

    add install script

---
+++ install/install.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=target/xz-ajiaedu-common-3.0-SNAPSHOT.jar
---
+++ install/install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mvn install:install-file -Dfile=target/xz-ajiaedu-common-3.0-SNAPSHOT.jar
